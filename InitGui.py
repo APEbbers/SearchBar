9608abc4616fa65f7b83c4d528ab8dcd1d29d760
APEbbers <paul.ebbers@gmail.com>
2025-09-06 10:25:52 +0200

    test with widget at mouse - 1

--- InitGui.py
+++ InitGui.py
@@ -1,5 +1,8 @@
 import FreeCAD as App
 import FreeCADGui as Gui
+from PySide6.QtWidgets import QToolBar, QMenu
+from PySide6.QtGui import QShortcut, QKeySequence, QCursor
+from PySide6.QtCore import Qt, Signal, QEvent, QObject
 
 # Avoid garbage collection by storing the action in a global variable
 wax = None
@@ -18,9 +21,9 @@ def addToolSearchBox():
     global wax, sea, tbr
     mw = Gui.getMainWindow()
     import SearchBox
-    from PySide.QtWidgets import QToolBar
-    from PySide.QtGui import QShortcut, QKeySequence
-
+    from EventFilters import EventInspector
+    from PySide6.QtWidgets import QToolBar
+    
     if mw:
         if sea is None:
             wax = SearchBox.SearchBoxFunction(mw)
@@ -31,9 +34,29 @@ def addToolSearchBox():
             tbr.setObjectName("SearchBar")
             tbr.addAction(wax)
         mw.addToolBar(tbr)
+        mw.installEventFilter(EventInspector(mw))
         tbr.show()
         return
+    
+# def ShowSearchBoxAtCursor():
+#     global wax, sea, tbr
+#     mw = Gui.getMainWindow()
+#     tbr = customToolbar("mouseMenu")
+#     import SearchBox
 
+#     if mw:
+#         if sea is None:
+#             wax = SearchBox.SearchBoxFunction(mw)
+#         if tbr is None:
+#             tbr = QToolBar("SearchBar")  # QtGui.QDockWidget()
+#             # Include FreeCAD in the name so that one can find windows labeled with
+#             # FreeCAD easily in window managers which allow search through the list of open windows.
+#             tbr.setObjectName("SearchBar")
+#             tbr.addAction(wax)
+#         return
+    
 
 addToolSearchBox()
+# ShowSearchBoxAtCursor()
 Gui.getMainWindow().workbenchActivated.connect(addToolSearchBox)
+
