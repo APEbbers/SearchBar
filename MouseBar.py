2bc28c375592f06e777baad6b3d6d3f4e2088efb
APEbbers <paul.ebbers@gmail.com>
2025-09-08 18:13:41 +0200

    test with widget at mouse - 4

--- MouseBar.py
+++ MouseBar.py
@@ -1,10 +1,10 @@
+from tkinter import Menubutton
 from turtle import isvisible
 import FreeCAD as App
 import FreeCADGui as Gui
-from PySide6.QtWidgets import QToolBar, QMenu
+from PySide6.QtWidgets import QToolBar, QMenu, QHBoxLayout, QWidget, QWidgetAction
 from PySide6.QtGui import QShortcut, QKeySequence, QCursor
 from PySide6.QtCore import Qt, Signal, QEvent, QObject
-
 # Avoid garbage collection by storing the action in a global variable
 wax = None
 sea = None
@@ -14,59 +14,41 @@ tbr = None
 translate = App.Qt.translate
 
 class EventInspector(QObject):
-    visible = False
     
     def __init__(self, parent):
         super(EventInspector, self).__init__(parent)
-        self.tbr = self.ShowSearchBoxAtCursor()
 
     def eventFilter(self, obj, event):
-        if event.type() == QEvent.Type.KeyPress:
-            menu = QMenu("test")
-            if event.key() == Qt.Key.Key_S and self.visible is False:
-                pos = QCursor.pos()
-                menu.addActions(self.tbr.actions())
-                menu.exec_(menu.mapToGlobal(pos))
-                menu.setVisible(True)
-                self.visible = True
-                print(event)
-                # return QObject.eventFilter(self, obj, event)
-        return False
-    
-    def ShowSearchBoxAtCursor(self):
-        global wax, sea, tbr
         mw = Gui.getMainWindow()
-        tbr = QToolBar("mouseMenu")
-        import SearchBox
+        
+        if event.type() == QEvent.Type.KeyPress:
+            menu = QMenu("test", parent=mw)            
+            if event.key() == Qt.Key.Key_S:
+                pos = QCursor.pos()
+                action = ToolBarAction(mw)
+                menu.addAction(action)
+                action.setEnabled(True)
+                menu.exec_(menu.mapToGlobal(pos))
+                return True
+        return False
 
-        if mw:
-            wax = SearchBox.SearchBoxFunction(mw)
-            tbr = QToolBar("mouseMenu")  # QtGui.QDockWidget()
-            # Include FreeCAD in the name so that one can find windows labeled with
-            # FreeCAD easily in window managers which allow search through the list of open windows.
-            tbr.setObjectName("mouseMenu")
-            tbr.addAction(wax)
-        return tbr
-    
-class customToolbar(QToolBar):
-    keyPressed = Signal(QEvent)
-    
-    def __init__(self, text):
-        super(customToolbar, self).__init__()
-        self.keyPressed.connect(self.on_key)
+class ToolBarAction(QWidgetAction):
+    import SearchBox
+    mw = Gui.getMainWindow()
+    action: QWidgetAction = SearchBox.SearchBoxFunction(mw)
         
-    def keyPressEvent(self, event):
-        # if event.key() == Qt.Key.Key_Escape:
-        #     print("Killing")
-        #     self.deleteLater()
-        # elif event.key() == Qt.Key.Key_S:
-        #     # pos = QCursor.pos()
-        #     self.setMouseTracking(True)
-        #     self.exec_(self.mapToGlobal(event.pos()))
-        self.keyPressed.emit(event)
-        event.accept()
+    toolbar = QToolBar()
+    toolbar.setObjectName("mouseMenu")
+    toolbar.addAction(action)
+    
+    def __init__(self, parent):
+        super(ToolBarAction, self).__init__(parent)
         
-    def on_key(self, event):
-        print('event received @ myDialog')
-        if event.key() == Qt.Key.Key_0:
-            print(0)
+        layout = QHBoxLayout()
+        self.widget = QWidget()
+        layout.addWidget(self.toolbar)
+        self.widget.setLayout(layout)
+
+        self.setDefaultWidget(self.widget)
+        return
+    
