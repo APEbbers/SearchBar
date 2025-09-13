15f0127c0e150621360198bebc88d11e76d9bf50
APEbbers <paul.ebbers@gmail.com>
2025-09-13 13:16:09 +0200

    Update changelog ui

--- Resources/ui/ui_ChangeDialog.py
+++ Resources/ui/ui_ChangeDialog.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'ChangeDialogRmjbFA.ui'
+## Form generated from reading UI file 'ChangeDialogmslKts.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.9.2
 ##
@@ -15,8 +15,8 @@ from PySide.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide.QtWidgets import (QApplication, QCheckBox, QGridLayout, QSizePolicy,
-    QTextEdit, QWidget)
+from PySide.QtWidgets import (QApplication, QCheckBox, QFrame, QGridLayout,
+    QSizePolicy, QTextEdit, QWidget)
 
 class Ui_Form(object):
     def setupUi(self, Form):
@@ -32,6 +32,8 @@ class Ui_Form(object):
 
         self.textEdit = QTextEdit(Form)
         self.textEdit.setObjectName(u"textEdit")
+        self.textEdit.setFrameShape(QFrame.Shape.NoFrame)
+        self.textEdit.setTextInteractionFlags(Qt.TextInteractionFlag.NoTextInteraction)
 
         self.gridLayout.addWidget(self.textEdit, 0, 0, 1, 1)
 
@@ -44,6 +46,7 @@ class Ui_Form(object):
     def retranslateUi(self, Form):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
         self.DoNotShowAgain.setText(QCoreApplication.translate("Form", u"Do not show again", None))
+        self.textEdit.setMarkdown("")
         self.textEdit.setHtml(QCoreApplication.translate("Form", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
@@ -51,6 +54,6 @@ class Ui_Form(object):
 "li.unchecked::marker { content: \"\\2610\"; }\n"
 "li.checked::marker { content: \"\\2612\"; }\n"
 "</style></head><body style=\" font-family:'Noto Sans'; font-size:10pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><img src=\":/Images/Resources/Images/2025-09-11 Change shortcut.png\" /></p></body></html>", None))
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", None))
     # retranslateUi
 
