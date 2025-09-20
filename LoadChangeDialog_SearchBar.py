25dc211ac23ad94a9259e03b85723fab35c37722
APEbbers <paul.ebbers@gmail.com>
2025-09-20 22:38:07 +0200

    Updated changelog text

--- LoadChangeDialog_SearchBar.py
+++ LoadChangeDialog_SearchBar.py
@@ -34,14 +34,18 @@ class LoadDialog(ui_ChangeDialog.Ui_Form, QObject):
         + "## Filter options  \n"
         + "The results of the searchbar consists of three type of results: Buttons, parameters and open documents with their content.  \n"
         + "It is now possible to enable or disable one or more of these result types in the preference page.  \n"
-        + f'<img src=\"{os.path.join(pathImages, "FilterOptions.png")}\"/>  \n'
+        + f'<img src=\"{os.path.join(pathImages, "FilterOptions.png")}\" width=500/>  \n'
         + "###\n"
         + "## 3D Viewer improvements  \n"
         + "a 3D Previewer can be enabled to for open documents. With this new release the following changes and improvements are made: \n"
         + "- The button to enable this permanently is moved from the results dialog to preferences. \n"
         + f'<img src=\"{os.path.join(pathImages, "Enable 3D Previewer in preferences.png")}\" width=500/>  \n'
         + "- When the 3D preview is disabled, a thumbnail is now added instead. \n"
-        + "  For this to work, a thumbnail must be present in the document. You can enable this in preferences->General->Documents.  \n"
+        + "  For this to work, a thumbnail must be present in the document. \n"
+        + "  You can enable this in preferences->General->Documents.  \n"
+        + "- The navigation cube is no removed from the 3D preview. The cube was too big and was partially hiding the document.  \n"
+        + "- The 3D preview is now set to the default orientation of the document, when it is first shown.\n"
+        + "  For most documents this will be the Isometric view.  \n"
     )
     
     # Enter the version for which the form must show on startup
