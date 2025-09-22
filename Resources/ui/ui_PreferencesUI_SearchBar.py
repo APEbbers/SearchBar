e448c38d7844fd3946db2675b2ae75e252620fd8
APEbbers <paul.ebbers@gmail.com>
2025-09-22 21:55:19 +0200

    test with issue pref page - 2

--- Resources/ui/ui_PreferencesUI_SearchBar.py
+++ Resources/ui/ui_PreferencesUI_SearchBar.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'PreferencesUI_SearchBarORcAGS.ui'
+## Form generated from reading UI file 'PreferencesUI_SearchBarwsULbe.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.9.2
 ##
