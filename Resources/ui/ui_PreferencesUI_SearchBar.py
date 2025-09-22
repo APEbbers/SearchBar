2b19f30a638aad62aa4ac294a5db484cc1bdb114
APEbbers <paul.ebbers@gmail.com>
2025-09-22 20:32:56 +0200

    test with issue pref page

--- Resources/ui/ui_PreferencesUI_SearchBar.py
+++ Resources/ui/ui_PreferencesUI_SearchBar.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'PreferencesUI_SearchBarhOajHi.ui'
+## Form generated from reading UI file 'PreferencesUI_SearchBarORcAGS.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.9.2
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
     QMetaObject, QObject, QPoint, QRect,
     QSize, QTime, QUrl, Qt)
-from PySide.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide.QtWidgets import (QApplication, QFrame, QGridLayout, QGroupBox,
+from PySide6.QtWidgets import (QApplication, QFrame, QGridLayout, QGroupBox,
     QLabel, QSizePolicy, QSpacerItem, QWidget)
 
 class Ui_Form(object):
